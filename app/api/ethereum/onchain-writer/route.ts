51dbddbd420510678681fed027806bc09ea2d3c0
chiragbadhe <badhechirag633@gmail.com>
2025-07-19 00:46:36 +0530

    fix: temp fix

--- app/api/ethereum/onchain-writer/route.ts
+++ app/api/ethereum/onchain-writer/route.ts
@@ -60,11 +60,11 @@ async function verifyParagraphPost(address: Address): Promise<[boolean]> {
       throw new Error(`GraphQL request failed: ${response.statusText}`);
     }
 
-    // const posts = response.data?.data?.transactions?.edges || [];
-    // const postCount = posts.length;
-    // const hasPosted = postCount > 0;
+    const posts = response.data?.data?.transactions?.edges || [];
+    const postCount = posts.length;
+    const hasPosted = postCount > 0;
 
-    return [true]; //temp fix
+    return [hasPosted];
   } catch (error) {
     console.error("Error verifying Paragraph post:", error);
     return [false];
